aaf70746996e720785bddf57a015ece9262e65dd
Rui Marinho <me@ruimarinho.net>
2020-03-14 23:29:38 +0000

    Refactor to use CameraViewModel

---
+++ src/CoBuster/CoBuster/ViewModels/CameraViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CoBuster.ViewModels
+{
+	public class CameraViewModel : BaseViewModel
+	{
+		VitalSignsProcessing _vitalSignsProcessing;
+
+		public Command StartMeasuringCommand { get; set; }
+		public CameraViewModel()
+		{
+			Title = "CAMERA";
+			Instructions = "Please cover your phone camera with your index finger. Wait around for 40 seconds to get result";
+			StartMeasuringCommand = new Command(() => StarMeasuring());
+		}
+
+		void StarMeasuring()
+		{
+			_vitalSignsProcessing = new VitalSignsProcessing();
+			CameraVisible = true;
+			InstructionsVisible = false;
+		}
+
+		internal void PreviewCallback(byte[] data, Size size)
+		{
+			if (_vitalSignsProcessing == null)
+				return;
+
+			//we need to handle errors here
+			var o2 = _vitalSignsProcessing.O2Processsing(data, size);
+
+			if (o2 > 0)
+			{
+				Instructions = $"Your results for O2 saturation is {o2}";
+				CameraVisible = false;
+				InstructionsVisible = true;
+			}
+
+		}
+
+		string instructions = string.Empty;
+		public string Instructions
+		{
+			get { return instructions; }
+			set { SetProperty(ref instructions, value); }
+		}
+
+
+		bool _instructionsVisible = true;
+		public bool InstructionsVisible
+		{
+			get { return _instructionsVisible; }
+			set { SetProperty(ref _instructionsVisible, value); }
+		}
+
+		bool _cameraVisible = false;
+		public bool CameraVisible
+		{
+			get { return _cameraVisible; }
+			set { SetProperty(ref _cameraVisible, value); }
+		}
+	}
+}
--- src/CoBuster/CoBuster/Views/CameraPage.xaml.cs
+++ src/CoBuster/CoBuster/Views/CameraPage.xaml.cs
@@ -1,64 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using CoBuster.ViewModels;
 using Xamarin.Forms;
-using static CoBuster.Controls.CameraView;
 
 namespace CoBuster.Views
 {
 	public partial class CameraPage : ContentPage
 	{
-		VitalSignsProcessing vitalSignsProcessing = new VitalSignsProcessing();
+		CameraViewModel viewModel;
 
 		public CameraPage()
 		{
 			InitializeComponent();
 
-			cameraView.CameraOption = CameraOptions.Default;
-			cameraView.FlashMode = CameraFlashMode.On;
-			cameraView.MediaCaptured += (_, e) =>
-			{
-				// PreviewCallback(e.Image);
-				switch (cameraView.CaptureOptions)
-				{
-					default:
-					case CameraCaptureOptions.Default:
-					case CameraCaptureOptions.Photo:
-						//testMediaElement.IsVisible = false;
-						//testImage.IsVisible = true;
-						//testImage.Source = e.Image;
-						buttonShot.Text = "Shot";
-						break;
-					case CameraCaptureOptions.Video:
-						//testImage.IsVisible = false;
-						//testMediaElement.IsVisible = true;
-						//testMediaElement.Source = e.Video;
-						buttonShot.Text = "Start record";
-						break;
-				}
-			};
+			BindingContext = viewModel = new CameraViewModel();
 
-			cameraView.OnAvailable += (_, available) =>
-			{
-				if (available)
-				{
-				}
-				buttonShot.IsEnabled = available;
-			};
-		}
+			cameraView.OnAvailable += (_, available) => btnStart.IsEnabled = available;
 
-		void PreviewCallback(byte[] data, Size size)
-		{
-			//we need to handle errors here
-			var o2 = vitalSignsProcessing.O2Processsing(data, size);
+			cameraView.PreviewAvailabe += (_, e) =>
+					viewModel.PreviewCallback(e.Data, new Size(cameraView.Width, cameraView.Height));
 
-			if (o2 != 0)
-				DisplayAlert("o2 results", o2.ToString(), "ok");
-		}
-
-		void buttonShot_Clicked(System.Object sender, System.EventArgs e)
-		{
-			cameraView.Shutter();
 		}
 	}
 }
