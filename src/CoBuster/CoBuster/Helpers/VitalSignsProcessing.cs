359405d5ee098e21ee940ca636cd5176df57b63f
Rui Marinho <me@ruimarinho.net>
2020-03-14 17:08:40 +0000

    Initial process of O2

---
+++ src/CoBuster/CoBuster/Helpers/VitalSignsProcessing.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace CoBuster
+{
+	public class VitalSignsProcessing
+	{
+		int processing = 0;
+
+		//Freq + timer variable
+		static long startTime = 0;
+		double SamplingFreq;
+
+		// SPO2 variables
+		static double RedBlueRatio = 0;
+		double Stdr = 0;
+		double Stdb = 0;
+		double sumred = 0;
+		double sumblue = 0;
+		public int o2;
+
+		//Arraylist
+		public List<Double> RedAvgList = new List<Double>();
+		public List<Double> BlueAvgList = new List<Double>();
+		public int counter = 0;
+
+		public int O2Processsing(byte[] data, Size size)
+		{
+			if (Interlocked.Exchange(ref processing, 1) == 1)
+			{
+				return -1;
+			}
+
+			int width = (int)size.Width;
+			int height = (int)size.Height;
+			double RedAvg;
+			double BlueAvg;
+
+			RedAvg = ImageProcessing.DecodeYUV420SPtoRedBlueGreenAvg(data, height, width, 1); //1 stands for red intensity, 2 for blue, 3 for green
+			sumred = sumred + RedAvg;
+			BlueAvg = ImageProcessing.DecodeYUV420SPtoRedBlueGreenAvg(data, height, width, 2); //1 stands for red intensity, 2 for blue, 3 for green
+			sumblue = sumblue + BlueAvg;
+
+			RedAvgList.Add(RedAvg);
+			BlueAvgList.Add(BlueAvg);
+
+			++counter; //countes number of frames in 30 seconds
+
+			//To check if we got a good red intensity to process if not return to the condition and set it again until we get a good red intensity
+			if (RedAvg < 200)
+			{
+				//inc = 0;
+				//ProgP = inc;
+				//ProgO2.setProgress(ProgP);
+				Interlocked.Exchange(ref processing, 0);
+			}
+
+			long endTime = DateTime.Now.Millisecond;
+			double totalTimeInSecs = (endTime - startTime) / 1000d; //to convert time to seconds
+
+			if (totalTimeInSecs >= 30)
+			{ //when 30 seconds of measuring passes do the following " we chose 30 seconds to take half sample since 60 seconds is normally a full sample of the heart beat
+
+				startTime = DateTime.Now.Millisecond;
+				SamplingFreq = (counter / totalTimeInSecs);
+				Double[] Red = RedAvgList.ToArray();
+				Double[] Blue = BlueAvgList.ToArray();
+				//MathNet.Numerics.IntegralTransforms.Fourier.FrequencyScale(Red, counter, MathNet.Numerics.IntegralTransforms.FourierOptions.Default);
+				double HRFreq = 1.0; //;  Fft.FFT(Red, counter, SamplingFreq);
+				double bpm = (int)Math.Ceiling(HRFreq * 60);
+
+				double meanr = sumred / counter;
+				double meanb = sumblue / counter;
+
+				for (int i = 0; i < counter - 1; i++)
+				{
+
+					Double bufferb = Blue[i];
+
+					Stdb = Stdb + ((bufferb - meanb) * (bufferb - meanb));
+
+					Double bufferr = Red[i];
+
+					Stdr = Stdr + ((bufferr - meanr) * (bufferr - meanr));
+
+				}
+
+				double varr = Math.Sqrt(Stdr / (counter - 1));
+				double varb = Math.Sqrt(Stdb / (counter - 1));
+
+				double R = (varr / meanr) / (varb / meanb);
+
+				double spo2 = 100 - 5 * (R);
+				o2 = (int)(spo2);
+
+				if ((o2 < 80 || o2 > 99) || (bpm < 45 || bpm > 200))
+				{
+					//inc = 0;
+					//ProgP = inc;
+					//ProgO2.setProgress(ProgP);
+					//mainToast = Toast.makeText(getApplicationContext(), "Measurement Failed", Toast.LENGTH_SHORT);
+					//mainToast.show();
+					//startTime = System.currentTimeMillis();
+					//counter = 0;
+					Interlocked.Exchange(ref processing, 0);
+					return -2;
+				}
+
+			}
+
+			if (o2 != 0)
+			{
+				
+
+			}
+
+			if (RedAvg != 0)
+			{
+				//                    ProgP=inc++/34;;
+				//                    ProgO2.setProgress(ProgP);
+			}
+
+			Interlocked.Exchange(ref processing, 0);
+
+			return o2;
+		}
+	}
+}
--- src/CoBuster/CoBuster/Views/CameraPage.xaml.cs
+++ src/CoBuster/CoBuster/Views/CameraPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading;
 using Xamarin.Forms;
 using static CoBuster.Controls.CameraView;
 
@@ -8,47 +8,57 @@ namespace CoBuster.Views
 {
 	public partial class CameraPage : ContentPage
 	{
+		VitalSignsProcessing vitalSignsProcessing = new VitalSignsProcessing();
+
 		public CameraPage()
 		{
 			InitializeComponent();
 
 			cameraView.CameraOption = CameraOptions.Default;
-            cameraView.FlashMode = CameraFlashMode.On;
-            cameraView.MediaCaptured += (_, e) =>
-            {
-                switch (cameraView.CaptureOptions)
-                {
-                    default:
-                    case CameraCaptureOptions.Default:
-                    case CameraCaptureOptions.Photo:
-                        //testMediaElement.IsVisible = false;
-                        //testImage.IsVisible = true;
-                        //testImage.Source = e.Image;
-                        buttonShot.Text = "Shot";
-                        break;
-                    case CameraCaptureOptions.Video:
-                        //testImage.IsVisible = false;
-                        //testMediaElement.IsVisible = true;
-                        //testMediaElement.Source = e.Video;
-                        buttonShot.Text = "Start record";
-                        break;
-                }
-            };
+			cameraView.FlashMode = CameraFlashMode.On;
+			cameraView.MediaCaptured += (_, e) =>
+			{
+				// PreviewCallback(e.Image);
+				switch (cameraView.CaptureOptions)
+				{
+					default:
+					case CameraCaptureOptions.Default:
+					case CameraCaptureOptions.Photo:
+						//testMediaElement.IsVisible = false;
+						//testImage.IsVisible = true;
+						//testImage.Source = e.Image;
+						buttonShot.Text = "Shot";
+						break;
+					case CameraCaptureOptions.Video:
+						//testImage.IsVisible = false;
+						//testMediaElement.IsVisible = true;
+						//testMediaElement.Source = e.Video;
+						buttonShot.Text = "Start record";
+						break;
+				}
+			};
 
-            cameraView.OnAvailable += (_, available) =>
-            {
-                if (available)
-                {
-                }
-                buttonShot.IsEnabled = available;
-            };
-        }
+			cameraView.OnAvailable += (_, available) =>
+			{
+				if (available)
+				{
+				}
+				buttonShot.IsEnabled = available;
+			};
+		}
 
+		void PreviewCallback(byte[] data, Size size)
+		{
+			//we need to handle errors here
+			var o2 = vitalSignsProcessing.O2Processsing(data, size);
 
+			if (o2 != 0)
+				DisplayAlert("o2 results", o2.ToString(), "ok");
+		}
 
-        void buttonShot_Clicked(System.Object sender, System.EventArgs e)
-        {
-            cameraView.Shutter();
-        }
-    }
+		void buttonShot_Clicked(System.Object sender, System.EventArgs e)
+		{
+			cameraView.Shutter();
+		}
+	}
 }
